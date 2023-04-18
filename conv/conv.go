7d1eca8b2c626b4379db29e4ce33747f2ad05bcd
HusseinSultan <hosen.sol.hs90@gmail.com>
2023-04-18 23:29:00 +0200

    endring

--- conv/conv.go
+++ conv/conv.go
@@ -1,34 +1,41 @@
 package conv
 
 /*
-  I denne pakken skal alle konverteringfunksjonene
-  implementeres. Bruk engelsk.
-    FarhenheitToCelsius
+  In this package, all conversion functions should be implemented.
+    FahrenheitToCelsius
     CelsiusToFahrenheit
-    KelvinToFarhenheit
-    ...
+    KelvinToFahrenheit
+    FahrenheitToKelvin
+    CelsiusToKelvin
+    KelvinToCelsius
 */
 
-// FarhenheitToCelsius converts a temperature in degrees Fahrenheit to degrees Celsius.
-func FarhenheitToCelsius(f float64) float64 {
-	return (f - 32) * 5 / 9
+// FahrenheitToCelsius converts a temperature in degrees Fahrenheit to degrees Celsius.
+func FahrenheitToCelsius(f float64) float64 {
+        return (f - 32) * 5 / 9
 }
 
 // CelsiusToFahrenheit converts a temperature in degrees Celsius to degrees Fahrenheit.
 func CelsiusToFahrenheit(c float64) float64 {
-	return c*9/5 + 32
+        return c*9/5 + 32
 }
 
-// KelvinToFarhenheit converts a temperature in Kelvin to degrees Fahrenheit.
-func KelvinToFarhenheit(k float64) float64 {
-	return (k-273.15)*9/5 + 32
+// KelvinToFahrenheit converts a temperature in Kelvin to degrees Fahrenheit.
+func KelvinToFahrenheit(k float64) float64 {
+        return (k-273.15)*9/5 + 32
 }
+
+// FahrenheitToKelvin converts a temperature in degrees Fahrenheit to Kelvin.
 func FahrenheitToKelvin(f float64) float64 {
-	return (f-32)*(5.0/9.0) + 273.15
+        return (f-32)*(5.0/9.0) + 273.15
 }
-func CelsiusToKelvin(C float64) float64 {
-	return (C + 273.15)
+
+// CelsiusToKelvin converts a temperature in degrees Celsius to Kelvin.
+func CelsiusToKelvin(c float64) float64 {
+        return c + 273.15
 }
-func KelvinToCelsius(K float64) float64 {
-	return (K - 273.15)
+
+// KelvinToCelsius converts a temperature in Kelvin to degrees Celsius.
+func KelvinToCelsius(k float64) float64 {
+        return k - 273.15
 }
