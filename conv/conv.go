aa22c59e7004e113ff8ff623088badf4a2e1e722
HusseinSultan <hosen.sol.hs90@gmail.com>
2023-04-24 11:46:10 +0200

    endring

--- conv/conv.go
+++ conv/conv.go
@@ -1,43 +1,31 @@
 package conv
 
-import "math"
+func FarhenheitToCelsius(value float64) float64 {
 
-/*
-	FahrenheitToCelsius
-	CelsiusToFahrenheit
-	KelvinToFahrenheit
-	CelsiusToKelvin
-	KelvinToCelsius
-	FahrenheitToKelvin
-*/
-
-// La til math.Round slik at output bare har 2 tall bak komma.
-func FahrenheitToCelsius(Fahrenheit float64) float64 {
-	Celsius := (Fahrenheit - 32) * 5 / 9
-	return math.Round(Celsius*100) / 100
+	return (value - 32) * 5 / 9
 }
 
-func CelsiusToFahrenheit(Celsius float64) float64 {
-	Fahrenheit := (Celsius * 1.8) + 32
-	return math.Round(Fahrenheit*100) / 100
+func CelsiusToFarhenheit(value float64) float64 {
+
+	return value*9/5 + 32
 }
 
-func KelvinToFahrenheit(Kelvin float64) float64 {
-	Fahrenheit := Kelvin*9/5 - 459.67
-	return math.Round(Fahrenheit*100) / 100
+func KelvinToCelcius(value float64) float64 {
+
+	return value - 273.15
 }
 
-func CelsiusToKelvin(Celsius float64) float64 {
-	Kelvin := Celsius + 273.15
-	return math.Round(Kelvin*100) / 100
+func CelsiusToKelvin(value float64) float64 {
+
+	return value + 273.15
 }
 
-func KelvinToCelsius(Kelvin float64) float64 {
-	Celsius := Kelvin - 273.15
-	return math.Round(Celsius*100) / 100
+func KelvinToFarhenheit(value float64) float64 {
 
+	return (value-273.15)*9/5 + 32
 }
-func FahrenheitToKelvin(Fahrenheit float64) float64 {
-	Kelvin := (Fahrenheit + 459.67) * 5 / 9
-	return math.Round(Kelvin*100) / 100
+
+func FarhenheitToKelvin(value float64) float64 {
+
+	return (value-32)*5/9 + 273.15
 }
--- conv/conv_test.go
+++ conv/conv_test.go
@@ -1,45 +1,33 @@
 package conv
 
 import (
-	"reflect"
+	"math"
 	"testing"
 )
 
 /*
-*
 	Mal for testfunksjoner
 	Du skal skrive alle funksjonene basert på denne malen
 	For alle konverteringsfunksjonene (tilsammen 6)
 	kan du bruke malen som den er (du må selvsagt endre
 	funksjonsnavn og testverdier)
 */
-func TestFarhenheitToCelsius(t *testing.T) {
-	type test struct {
-		input float64
-		want  float64
+
+func withinTolerance(a, b, error float64) bool {
+
+	if a == b {
+		return true
 	}
 
-	tests := []test{
-		{input: 134, want: 56.67},
+	difference := math.Abs(a - b)
+
+	if b == 0 {
+		return difference < error
 	}
 
-	for _, tc := range tests {
-		got := FarhenheitToCelsius(tc.input)
-		if !reflect.DeepEqual(tc.want, got) {
-			t.Errorf("expected: %v, got: %v", tc.want, got)
-		}
-	}
+	return (difference / math.Abs(b)) < error
 }
 
-// De andre testfunksjonene implementeres her
-// ...
-package conv
-
-import (
-	"reflect"
-	"testing"
-)
-
 func TestFarhenheitToCelsius(t *testing.T) {
 	type test struct {
 		input float64
@@ -48,13 +36,11 @@ func TestFarhenheitToCelsius(t *testing.T) {
 
 	tests := []test{
 		{input: 134, want: 56.67},
-		{input: 32, want: 0},
-		{input: -40, want: -40},
 	}
 
 	for _, tc := range tests {
 		got := FarhenheitToCelsius(tc.input)
-		if !reflect.DeepEqual(tc.want, got) {
+		if !withinTolerance(tc.want, got, 1e-2) {
 			t.Errorf("expected: %v, got: %v", tc.want, got)
 		}
 	}
@@ -68,150 +54,79 @@ func TestCelsiusToFarhenheit(t *testing.T) {
 
 	tests := []test{
 		{input: 56.67, want: 134},
-		{input: 0, want: 32},
-		{input: -40, want: -40},
 	}
-
 	for _, tc := range tests {
 		got := CelsiusToFarhenheit(tc.input)
-		if !reflect.DeepEqual(tc.want, got) {
+		if !withinTolerance(tc.want, got, 1e-2) {
 			t.Errorf("expected: %v, got: %v", tc.want, got)
 		}
 	}
 }
 
-func TestMetersToFeet(t *testing.T) {
+func TestKelvinToCelcius(t *testing.T) {
 	type test struct {
 		input float64
 		want  float64
 	}
 
 	tests := []test{
-		{input: 1, want: 3.28084},
-		{input: 0, want: 0},
-		{input: -10, want: -32.8084},
+		{input: 329.82, want: 56.67},
 	}
-
 	for _, tc := range tests {
-		got := MetersToFeet(tc.input)
-		if !reflect.DeepEqual(tc.want, got) {
+		got := KelvinToCelcius(tc.input)
+		if !withinTolerance(tc.want, got, 1e-2) {
 			t.Errorf("expected: %v, got: %v", tc.want, got)
 		}
 	}
 }
 
-func TestFeetToMeters(t *testing.T) {
+func TestCelciusToKelvin(t *testing.T) {
 	type test struct {
 		input float64
 		want  float64
 	}
 
 	tests := []test{
-		{input: 3.28084, want: 1},
-		{input: 0, want: 0},
-		{input: -32.8084, want: -10},
+		{input: 56.67, want: 329.82},
 	}
-
 	for _, tc := range tests {
-		got := FeetToMeters(tc.input)
-		if !reflect.DeepEqual(tc.want, got) {
+		got := CelsiusToKelvin(tc.input)
+		if !withinTolerance(tc.want, got, 1e-2) {
 			t.Errorf("expected: %v, got: %v", tc.want, got)
 		}
 	}
 }
 
-func TestKilogramsToPounds(t *testing.T) {
+func TestKelvinToFarhenheit(t *testing.T) {
 	type test struct {
 		input float64
 		want  float64
 	}
 
 	tests := []test{
-		{input: 1, want: 2.20462},
-		{input: 0, want: 0},
-		{input: -5, want: -11.0231},
+		{input: 329.82, want: 134.00},
 	}
-
 	for _, tc := range tests {
-		got := KilogramsToPounds(tc.input)
-		if !reflect.DeepEqual(tc.want, got) {
+		got := KelvinToFarhenheit(tc.input)
+		if !withinTolerance(tc.want, got, 1e-2) {
 			t.Errorf("expected: %v, got: %v", tc.want, got)
 		}
 	}
 }
 
-func TestPoundsToKilograms(t *testing.T) {
+func TestFarhenheitToKelvin(t *testing.T) {
 	type test struct {
 		input float64
 		want  float64
 	}
 
 	tests := []test{
-		{input: 2.20462, want:
-			package conv
-
-			import (
-				"reflect"
-				"testing"
-			)
-			
-			func TestPoundsToKilograms(t *testing.T) {
-				type test struct {
-					input float64
-					want  float64
-				}
-			
-				tests := []test{
-					{input: 2.20462, want: 1},
-					{input: 0, want: 0},
-					{input: -11.0231, want: -5},
-				}
-			
-				for _, tc := range tests {
-					got := PoundsToKilograms(tc.input)
-					if !reflect.DeepEqual(tc.want, got) {
-						t.Errorf("expected: %v, got: %v", tc.want, got)
-					}
-				}
-			}
-			
-			func TestGallonsToLiters(t *testing.T) {
-				type test struct {
-					input float64
-					want  float64
-				}
-			
-				tests := []test{
-					{input: 1, want: 3.78541},
-					{input: 0, want: 0},
-					{input: -5, want: -18.9271},
-				}
-			
-				for _, tc := range tests {
-					got := GallonsToLiters(tc.input)
-					if !reflect.DeepEqual(tc.want, got) {
-						t.Errorf("expected: %v, got: %v", tc.want, got)
-					}
-				}
-			}
-			
-			func TestLitersToGallons(t *testing.T) {
-				type test struct {
-					input float64
-					want  float64
-				}
-			
-				tests := []test{
-					{input: 3.78541, want: 1},
-					{input: 0, want: 0},
-					{input: -18.9271, want: -5},
-				}
-			
-				for _, tc := range tests {
-					got := LitersToGallons(tc.input)
-					if !reflect.DeepEqual(tc.want, got) {
-						t.Errorf("expected: %v, got: %v", tc.want, got)
-					}
-				}
-			}
-			
+		{input: 134.00, want: 329.82},
+	}
+	for _, tc := range tests {
+		got := FarhenheitToKelvin(tc.input)
+		if !withinTolerance(tc.want, got, 1e-2) {
+			t.Errorf("expected: %v, got: %v", tc.want, got)
+		}
+	}
+}
